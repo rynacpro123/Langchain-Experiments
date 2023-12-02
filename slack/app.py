5ee272cb80cee5216dec2346664a64578368e35c
rynacpro123 <rynacpro+github@gmail.com>
2023-12-01 21:39:05 -0800

    testcommit

--- slack/app.py
+++ slack/app.py
@@ -76,6 +76,7 @@ def handle_mentions(body, say):
 
 
 @flask_app.route("/slack/events", methods=["POST"])
+@require_slack_verification
 def slack_events():
     """
     Route for handling Slack events.
@@ -87,6 +88,35 @@ def slack_events():
     return handler.handle(request)
 
 
+signature_verifier = SignatureVerifier(SLACK_SIGNING_SECRET)
+
+def require_slack_verification(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not verify_slack_request():
+            abort(403)
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def verify_slack_request():
+    # Get the request headers
+    timestamp = request.headers.get("X-Slack-Request-Timestamp", "")
+    signature = request.headers.get("X-Slack-Signature", "")
+
+    # Check if the timestamp is within five minutes of the current time
+    current_timestamp = int(time.time())
+    if abs(current_timestamp - int(timestamp)) > 60 * 5:
+        return False
+
+    # Verify the request signature
+    return signature_verifier.is_valid(
+        body=request.get_data().decode("utf-8"),
+        timestamp=timestamp,
+        signature=signature,
+    )
+
 # Run the Flask app
 if __name__ == "__main__":
-    flask_app.run()
+    flask_app.run(host="0.0.0.0", port=8000)
